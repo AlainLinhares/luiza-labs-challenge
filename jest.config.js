d2d9707acebf82b3f5929c913124d8e0f2593390
Alain Melo <arllmcomputacao@gmail.com>
2025-03-12 15:59:10 -0300

    Committing initial application structure

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.spec.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'json-summary'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/test/'],
+  collectCoverageFrom: ['src/**/*.ts'],
+};
